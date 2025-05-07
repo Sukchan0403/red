d41a527f09369db6ceb20b3b292882f3aeb5b9ed
Sukchan0403 <ydhkms0904@gmail.com>
2025-05-07 17:48:08 +0900

    추가

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
-st.title('이도경')
-st.header('this is header')
-st.subheader('this is subheader')
+col1,col2 = st.columns([2,3])
+
+with col1:
+    st.title('이도경')
+with col2:
+    st.title('여철준')
+    st.checkbox('양정우')
+
+col1.subheader('나는 철이다')
+col2.checkbox('나는 금이다')
